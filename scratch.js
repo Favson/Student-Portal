9f487b265638ebc6b8976fc1a7b7fc5fb5144fdc
Adebisi <adebisimomoluwa@gmail.com>
2024-01-22 14:33:18 +0100

    student portal beginings

--- scratch.js
+++ scratch.js
@@ -5,22 +5,31 @@ function submit(){
     var lastName = document.getElementById("lastName").value
     var mailPhone = document.getElementById("mailPhone").value
     var password = document.getElementById("password").value
-
-    if (firstName === "" || lastName === "" || mailPhone === "" || password === "") {
-        errMsg.style.display = 'block'   
-        setTimeout(()=>{
-            errMsg.style.display= 'none'
-        }, 4000);
+    var confirmation = confirm("Are you Sure you want to Submit")
+    if (confirmation){
+        if (firstName === "" || lastName === "" || mailPhone === "" || password === "") {
+            errMsg.style.display = 'block'   
+            setTimeout(()=>{
+                errMsg.style.display= 'none'
+            }, 4000);
+        }else{
+            myTable.style.display = "block"
+            successmsg.style.display= "block"
+            setTimeout(()=>{
+                successmsg.style.display= "none"
+            }, 4000);
+            var studentObject = {firstName, lastName, mailPhone, password}
+            studentsInfo.push(studentObject)
+            document.getElementById('firstName').value = ''
+            document.getElementById('lastName').value = ''
+            document.getElementById('mailPhone').value = ''
+            document.getElementById('password').value = ''
+            studentList()
+        }
     }else{
-        myTable.style.display = "block"
-        var studentObject = {firstName, lastName, mailPhone, password}
-        studentsInfo.push(studentObject)
-        document.getElementById('firstName').value = ''
-        document.getElementById('lastName').value = ''
-        document.getElementById('mailPhone').value = ''
-        document.getElementById('password').value = ''
         studentList()
     }
+
 }
 
 
@@ -33,9 +42,10 @@ function studentList(){
                 <td class="col-1"><p>${x+1}</p></td>
                 <td class="col-4"><p>${studentsInfo[x].firstName} ${studentsInfo[x].lastName}</p></td>
                 <td class="col-4"><p>${studentsInfo[x].mailPhone}</p></td>
-                <td class="col-3"><button class="btn btn-danger" onclick="deleted(${x})">DELETE</button> <button class="btn btn-warning" onclick="iwanteidit(${studentsInfo[x].firstName} ${studentsInfo[x].lastName} ${studentsInfo[x].mailPhone})">Edit</button></td>
+                <td class="col-3"><button class="btn btn-danger" onclick="deleted(${x})">DELETE</button> <button class="btn btn-warning" onclick="iwanteidit(})">Edit</button></td>
             </tr>
         `
+        var studentArray =`${studentsInfo[x].firstName} ${studentsInfo[x].lastName} ${studentsInfo[x].mailPhone}`
     }
 }
 
